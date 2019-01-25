0b24ef2564e8a8b39a144ba5ebf316d56a26ef40
farley2904 <vashchishinya@gmail.com>
2019-01-26 00:35:38 +0200

    deleted adding image form  from  information page

--- resources/views/jbb/admin/information_content.blade.php
+++ resources/views/jbb/admin/information_content.blade.php
@@ -1,20 +1,4 @@
 {{-- <h4 class="card-title">{{$title}}</h4> --}}
-<div class="row">
-	<div class="col-md-6 ">
-		<form action="{{ route('admin.information.store') }}" method="post" enctype="multipart/form-data">
-			{{ csrf_field() }}
-			<div class="form-group">
-				<input type="file" name="image">
-				<button class="btn btn-default" tupe="submit">Загрузка</button>
-			</div>
-		</form>
-		@isset($path)
-			<img class="img-fluid" src="{{ asset('/storage/' . $path) }}">
-		@endisset
-	</div>
-</div>
-
-
 
 <div class="row">
 	<div class="col-md-6 ">

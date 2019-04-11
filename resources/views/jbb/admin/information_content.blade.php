ab27d7a7ff5331c406a7505eb8ae68b12585beda
farley2904 <vashchishinya@gmail.com>
2019-04-11 03:04:00 +0300

    edit information content

--- resources/views/jbb/admin/information_content.blade.php
+++ resources/views/jbb/admin/information_content.blade.php
@@ -14,114 +14,102 @@
 			</ul>
 
 			<div class="tab-content" id="langTabContent">
-			  <div class="tab-pane fade show active" id="ru" role="tabpanel" aria-labelledby="ru-tab">
+				<div class="tab-pane fade show active" id="ru" role="tabpanel" aria-labelledby="ru-tab">
+					<p class="h4 mb-2 mt-4">Общая</p>
+					 <!-- elfinder popup -->
+<!-- 				    <div class="mb-2">
+				    	<a  href="" class="popup_selector" data-inputid="logo_image">Загрузить логотип</a><br>
+				    	{{-- <img src="{{ asset(env('THEME')) }}/{{ old('logo_image') }}" alt=""  width="100px" height="100px"> --}}
+						{{-- <input type="hidden" id="logo_image" class="form-control mb-4" name="logo_image" value="{{old('logo_image')}}"> --}}
+				    </div> -->
+					<!-- Site name -->
+				    <label>Название:</label>
+				    <input type="text" id="" class="form-control mb-4" placeholder="Введите название сайта" name="name" value="{{old('name')}}">
 
-				<p class="h4 mb-2 mt-4">Общая</p>
-			     <!-- elfinder popup -->
-			    <div class="mb-2">
-			    	<a  href="" class="popup_selector" data-inputid="logo_image">Загрузить логотип</a><br>
-			    	<img src="{{ asset(env('THEME')) }}/{{ old('logo_image') }}" alt=""  width="100px" height="100px">
-					<input type="hidden" id="logo_image" class="form-control mb-4" name="logo_image" value="{{old('logo_image')}}">
-			    </div>
+				    <label>Слоган:</label>
+				    <input type="text" id="" class="form-control mb-4" placeholder="Введите cлоган сайта" name="slogan" value="{{old('slogan')}}">
 
+				    <label>O Нас:</label>
+				    <div class="form-group">
+				        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Информация о нас" name="about_us">{{old('about_us')}}</textarea>
+				    </div>
 
-			    <!-- Site name -->
-			    <label>Название:</label>
-			    <input type="text" id="" class="form-control mb-4" placeholder="Введите название сайта" name="name" value="{{old('name')}}">
+				   	<label>Мета-тег Description:</label>
+				    <div class="form-group">
+				        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Введите описание для поисковых систем" name="meta_description">{{old('meta_description')}}</textarea>
+				    </div>
 
-			    <label>Слоган:</label>
-			    <input type="text" id="" class="form-control mb-4" placeholder="Введите cлоган сайта" name="slogan" value="{{old('slogan')}}">
-
-			    <label>O Нас:</label>
-			    <div class="form-group">
-			        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Информация о нас" name="about_us">{{old('about_us')}}</textarea>
-			    </div>
-
-			   	<label>Мета-тег Description:</label>
-			    <div class="form-group">
-			        <textarea class="form-control rounded-0" id="exampleFormControlTextarea2" rows="3" placeholder="Введите описание для поисковых систем" name="meta_description">{{old('meta_description')}}</textarea>
-			    </div>
-
-			    <label></label>
-			    <p class="h4 mb-2">Время работы</p>
-			    <label>Пн-Сб</label>
-				<div class="row">
-					<div class="col">
-						<input type="time" class="form-control" id="worktime" name="worktime">
+				    <label></label>
+				    <p class="h4 mb-2">Время работы</p>
+				    <label>Пн-Сб</label>
+					<div class="row">
+						<div class="col">
+							<input type="time" class="form-control" id="worktime" name="worktime">
+						</div>
+						<div class="col">
+							<input type="time" class="form-control" id="worktime" name="worktime">
+						</div>
 					</div>
-					<div class="col">
-						<input type="time" class="form-control" id="worktime" name="worktime">
-					</div>
-				</div>
-			     
-			     <label>Вс</label>
-				<div class="row mb-4">
-					<div class="col">
-						<input type="time" class="form-control" id="worktime" name="worktime">
-					</div>
-					<div class="col">
-						<input type="time" class="form-control" id="worktime" name="worktime">
-					</div>
-				</div>			     
+				     
+				     <label>Вс</label>
+					<div class="row mb-4">
+						<div class="col">
+							<input type="time" class="form-control" id="worktime" name="worktime">
+						</div>
+						<div class="col">
+							<input type="time" class="form-control" id="worktime" name="worktime">
+						</div>
+					</div>			     
 
-			    <p class="h4 mb-2">Контакты</p>
-			    <!-- Adress -->
-			    <label>Адресс:</label>
-			    <input type="text" id="" class="form-control mb-4" placeholder="Адресс" name="adress" value="{{old('adress')}}">
+				    <p class="h4 mb-2">Контакты</p>
+				    <!-- Adress -->
+				    <label>Адресс:</label>
+				    <input type="text" id="" class="form-control mb-4" placeholder="Адресс" name="adress" value="{{old('adress')}}">
 
-			    <!-- Telephone -->
-			    <label>Телефоны:</label>
-			    <input type="text" id="" class="form-control mb-4" placeholder="Телефон 1" name="tel_1" value="{{old('tel_1')}}">
-			    <input type="text" id="" class="form-control mb-4" placeholder="Телефон 2" name="tel_2" value="{{old('tel_2')}}">
+				    <!-- Telephone -->
+				    <label>Телефоны:</label>
+				    <input type="text" id="" class="form-control mb-4" placeholder="Телефон 1" name="tel_1" value="{{old('tel_1')}}">
+				    <input type="text" id="" class="form-control mb-4" placeholder="Телефон 2" name="tel_2" value="{{old('tel_2')}}">
 
-			    <!-- Social -->
-			    <!-- <label>Соцсети</label> -->
-			    <div class="custom-control custom-checkbox mb-4">
-			        <input type="checkbox" class="custom-control-input" id="soc" name="social" value="on" {{ old('social') ? 'checked' : '' }} >
-			        <label class="custom-control-label" for="soc">Показать соцсети</label>
-			    </div>
-
-			    
-			  </div>
-
+				    <!-- Social -->
+				    <!-- <label>Соцсети</label> -->
+				    <div class="custom-control custom-checkbox mb-4">
+				        <input type="checkbox" class="custom-control-input" id="soc" name="social" value="on" {{ old('social') ? 'checked' : '' }} >
+				        <label class="custom-control-label" for="soc">Показать соцсети</label>
+				    </div>
+			  	</div>
 			  
-			  <div class="tab-pane fade" id="ua" role="tabpanel" aria-labelledby="ua-tab">
-			  	!!!УКРАЇНСЬКА!!!
+				<div class="tab-pane fade" id="ua" role="tabpanel" aria-labelledby="ua-tab">
+					<p class="h4 mb-2 mt-4">Загальна</p>
+					@if(isset($information))
+						@foreach ($information as $setting)
 
-
-
-			  	@if(isset($information))
-					@foreach ($information as $setting)
-
-						@if($setting->key == 'logo_img')
-
+							@if($setting->key == 'logo_img')
 								</br>{{ $setting->value }}
-						@endif
+							@endif
 
-						@if($setting->key == 'name')
+							@if($setting->key == 'name')
+									</br>{{ $setting->value }}
+							@endif
 
-								</br>{{ $setting->value }}
-						@endif
+						@endforeach
+					@endif
 
-					@endforeach
-				@endif
+				  	<!-- Default switch -->
+					<div class="custom-control custom-switch mb-4">
+					  <input type="checkbox" class="custom-control-input" id="customSwitches">
+					  <label class="custom-control-label" for="customSwitches"></label>
+					</div>
+					<select class="custom-select">
+					  <option selected>Open this select menu</option>
+					  <option value="1">One</option>
+					  <option value="2">Two</option>
+					  <option value="3">Three</option>
+					</select>
 
-			  	<!-- Default switch -->
-				<div class="custom-control custom-switch mb-4">
-				  <input type="checkbox" class="custom-control-input" id="customSwitches">
-				  <label class="custom-control-label" for="customSwitches"></label>
+					<label for="customRange1">Example range</label>
+					<input type="range" class="custom-range" id="customRange1">
 				</div>
-				<select class="custom-select">
-				  <option selected>Open this select menu</option>
-				  <option value="1">One</option>
-				  <option value="2">Two</option>
-				  <option value="3">Three</option>
-				</select>
-
-				<label for="customRange1">Example range</label>
-				<input type="range" class="custom-range" id="customRange1">
-
-			  </div>
 			</div>
 
 		    <!-- Update -->

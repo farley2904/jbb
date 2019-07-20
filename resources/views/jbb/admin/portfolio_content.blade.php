26b7d890921d36c28ef6fef98dad4a0490c9b5a2
farley2904 <vashchishinya@gmail.com>
2019-07-20 16:59:56 +0300

    add edit and destroy in portfolio_content.blade.php

--- resources/views/jbb/admin/portfolio_content.blade.php
+++ resources/views/jbb/admin/portfolio_content.blade.php
@@ -1,8 +1,19 @@
+<h2>Портфолио</h2>
 <div class="col">  
-  <h2>Портфолио</h2>
   <a href="{{route('admin.portfolio.create')}}" role="button" class="btn btn-success"><i class="fa fa-plus"></i></a>
 </div>
 <ol>
+  <li>
+    <div class="list-group-item border-0 d-flex justify-content-between">
+      <div>
+        <img src="https://jbb.com.ua/jbb/images/thumb.png" width=50px height=55px alt="portfolio">
+      </div>
+      <div>
+        <a href="{{route('admin.portfolio.destroy','id')}}" class="btn btn-danger float-right" role="button">Удалить</a>
+        <a href="{{route('admin.portfolio.edit','id')}}" class="btn btn-warning float-right" role="button">Изменить</a>
+      </div>
+    </div>
+  </li>
   <li>
     <div class="list-group-item border-0 d-flex justify-content-between">
       <div>

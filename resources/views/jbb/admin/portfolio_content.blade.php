c034e7a9603c2bab04287a56a441ed603ea93e8a
farley2904 <vashchishinya@gmail.com>
2019-07-25 13:39:57 +0300

    edit list in  portfolio_content

--- resources/views/jbb/admin/portfolio_content.blade.php
+++ resources/views/jbb/admin/portfolio_content.blade.php
@@ -1,27 +1,27 @@
 <h2>Портфолио</h2>
-<div class="col">  
-  <a href="{{route('admin.portfolio.create')}}" role="button" class="btn btn-success"><i class="fa fa-plus"></i></a>
+<div class="col" style="display: table;">  
+  <a href="{{route('admin.portfolio.create')}}" role="button" class="btn btn-success waves-effect waves-light pull-right mb-4"><i class="fa fa-plus"></i></a>
 </div>
 <ol>
   <li>
-    <div class="list-group-item border-0 d-flex justify-content-between">
+    <div class="list-group-item d-flex justify-content-between">
       <div>
         <img src="https://jbb.com.ua/jbb/images/thumb.png" width=50px height=55px alt="portfolio">
       </div>
       <div>
-        <a href="{{route('admin.portfolio.destroy','id')}}" class="btn btn-danger float-right" role="button">Удалить</a>
-        <a href="{{route('admin.portfolio.edit','id')}}" class="btn btn-warning float-right" role="button">Изменить</a>
+        <a href="{{route('admin.portfolio.destroy','id')}}" class="btn btn-danger float-right" role="button"><i class="fa fa-remove"></i></a>
+        <a href="{{route('admin.portfolio.edit','id')}}" class="btn btn-warning float-right" role="button"><i class="fa fa-edit"></i></a>
       </div>
     </div>
   </li>
   <li>
-    <div class="list-group-item border-0 d-flex justify-content-between">
+    <div class="list-group-item d-flex justify-content-between">
       <div>
         <img src="https://jbb.com.ua/jbb/images/thumb.png" width=50px height=55px alt="portfolio">
       </div>
       <div>
-        <a class="btn btn-danger float-right" role="button">Удалить</a>
-        <a class="btn btn-warning float-right" role="button">Изменить</a>
+        <a href="{{route('admin.portfolio.destroy','id')}}" class="btn btn-danger float-right" role="button"><i class="fa fa-remove"></i></a>
+        <a href="{{route('admin.portfolio.edit','id')}}" class="btn btn-warning float-right" role="button"><i class="fa fa-edit"></i></a>
       </div>
     </div>
   </li>
